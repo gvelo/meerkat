1bc8bc0a5f0fb6562e2751668a4d53ada50bce62
sebad78 <sebastian.dominguez@mercadolibre.com>
2019-05-30 19:47:20 -0300

    Saque el Timsort

--- segment/inmem/columns_test.go
+++ segment/inmem/columns_test.go
@@ -10,28 +10,23 @@ func TestColumnTimeStamp_Sort(t *testing.T) {
 
 	a := assert.New(t)
 	c := &ColumnTimeStamp{
-		data:    make([]int, 0),
-		prev:    0,
-		sorted:  false,
-		fInfo:   &segment.FieldInfo{0, "_time", segment.FieldTypeTimestamp, true},
-		sortMap: make([]int, 0)}
+		data:   make([]int, 0),
+		prev:   0,
+		sorted: false,
+		fInfo:  &segment.FieldInfo{0, "_time", segment.FieldTypeTimestamp, true}}
 
 	c.Add(123)
 	c.Add(455)
 	c.Add(34)
 	c.Add(500)
 
-	sm := c.SortMap()
-	a.Equal(1, sm[0])
-	a.Equal(2, sm[1])
-	a.Equal(3, sm[2])
-	a.Equal(4, sm[3])
+	a.Nil(c.SortMap())
 
 	c.Sort()
-
-	a.Equal(3, sm[0])
-	a.Equal(1, sm[1])
-	a.Equal(2, sm[2])
-	a.Equal(4, sm[3])
+	sm := c.SortMap()
+	a.Equal(2, sm[0])
+	a.Equal(0, sm[1])
+	a.Equal(1, sm[2])
+	a.Equal(3, sm[3])
 
 }
