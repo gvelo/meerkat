b4fdeb91cc7e5ad2433ba5c549b7c9fb8d504f4a
sebad78 <sebastian.dominguez@mercadolibre.com>
2019-06-28 09:43:46 -0300

    compile

--- internal/tools/encoding/encoder.go
+++ internal/tools/encoding/encoder.go
@@ -13,59 +13,70 @@
 
 package encoding
 
-type Encoder interface {
-	Encode(values interface{}) []byte
-	AppendNext(e Encoder) Encoder
-}
+import (
+	"fmt"
+)
 
-type RawEncoder struct {
-	next Encoder
-}
+type HandlerFunc func(...interface{})
 
-func (e *RawEncoder) Encode(values interface{}) []byte {
-	r := make([]byte, 0)
-	// nothing to do
-	switch values.(type) {
-	case []byte:
-		r = values.([]byte)
-	case []int:
-		r = UnsafeCastIntsToBytes(values.([]int))
-	case []float64:
-		r = UnsafeCastFloatsToBytes(values.([]float64))
-	case []string:
-		r = CastStringToBytes(values.([]string))
+type middleware func(HandlerFunc) HandlerFunc
+
+func BuildChain(f HandlerFunc, m ...middleware) HandlerFunc {
+	if len(m) == 0 {
+		return f
 	}
-	if e.next != nil {
-		return e.next.Encode(r)
-	} else {
-		return r
+	return m[0](BuildChain(f, m[1:cap(m)]...))
+}
+
+func main() {
+
+	var privateChain = []middleware{
+		Encoding,
+		Writer,
 	}
+
+	chain := BuildChain(WriteToFile, privateChain...)
+	chain("3", "3")
+
 }
 
-func (e *RawEncoder) AppendNext(ne Encoder) Encoder {
-	e.next = ne
-	return ne
-}
-
-type IntegerEncoder struct {
-	next Encoder
-}
-
-func (e *IntegerEncoder) Encode(values interface{}) []byte {
-	r := make([]byte, 0)
-	// nothing to do
-	switch values.(type) {
-	case []int:
-		r = UnsafeCastIntsToBytes(values.([]int))
-	}
-	if e.next != nil {
-		return e.next.Encode(r)
-	} else {
-		return r
+var Encoding = func(f HandlerFunc) HandlerFunc {
+	return func(args ...interface{}) {
+		fmt.Println("start Encoding")
+		f(args)
+		fmt.Println("end Encoding")
 	}
 }
 
-func (e *IntegerEncoder) AppendNext(ne Encoder) Encoder {
-	e.next = ne
-	return ne
+var Writer = func(f HandlerFunc) HandlerFunc {
+	return func(args ...interface{}) {
+		fmt.Println("start Writer")
+		f(args)
+		fmt.Println("end Writer")
+	}
+}
+
+var RawEncoder = func(f HandlerFunc) HandlerFunc {
+	return func(args ...interface{}) {
+		fmt.Println("start Writer")
+		r := make([]byte, 0)
+		// nothing to do
+		switch args[0].(type) {
+		case []byte:
+			r = args[0].([]byte)
+		case []int:
+			r = UnsafeCastIntsToBytes(args[0].([]int))
+		case []float64:
+			r = UnsafeCastFloatsToBytes(args[0].([]float64))
+		case []string:
+			r = CastStringToBytes(args[0].([]string))
+		}
+		f(r)
+	}
+}
+
+// this is the handler func we are wrapping with middlewares
+func WriteToFile(args ...interface{}) {
+
+	println("w,r")
 }
