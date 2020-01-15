062e05f9669594a941a133a72172ff5666c7d834
Gabriel Velo <gabriel.velo@gmail.com>
2020-01-15 00:03:11 -0300

    move btrie from old storage to the new package refactor btrie: now we use a map as a btrie container refactor btrie: support byte slice as a key add random testing

---
+++ internal/storage/index/inmem/posting_store.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Meerkat Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package inmem
+
+import "github.com/RoaringBitmap/roaring"
+
+// postingList holds the term posting list.
+type PostingList struct {
+	// offset on disk
+	Offset int
+
+	// the bitmap backing the list
+	Bitmap *roaring.Bitmap
+}
+
+func (posting *PostingList) Add(eventID uint32) {
+	posting.Bitmap.Add(eventID)
+}
+
+type PostingStore struct {
+	Store []*PostingList
+}
+
+func NewPostingStore() *PostingStore {
+	return &PostingStore{
+		Store: make([]*PostingList, 0),
+	}
+}
+
+func (s *PostingStore) NewPostingList(eventID uint32) *PostingList {
+	p := &PostingList{
+		Bitmap: roaring.New(),
+	}
+	p.Bitmap.Add(eventID)
+	s.Store = append(s.Store, p)
+	return p
+}
